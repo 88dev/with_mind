fa7b7714f47cbbf32d78cd93b44da48409ee51e8
[wmwwm88] <[wmwwm88@gmail.com]>
2022-06-30 17:56:04 +0900

    DB_report 시작

---
+++ 05.DB_report/DB_report.js
@@ -0,0 +1,9 @@
+// $(document).ready(function () {
+//     $(".main_tab_title li").click(function () {
+//         var idx = $(this).index();
+//         $(".main_tab_title li").removeClass("on");
+//         $(".main_tab_title li").eq(idx).addClass("on");
+//         $(".main_tab_cont > section").hide();
+//         $(".main_tab_cont > section").eq(idx).show();
+//     });
+// });
