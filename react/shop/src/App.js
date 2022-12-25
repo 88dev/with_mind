a22edcce4ea3f2be534ce47d72c0df4dae13f238
wmwwm88.dev <wmwwm88@gmail.com>
2022-12-26 00:49:51 +0900

    -c

--- react/shop/src/App.js
+++ react/shop/src/App.js
@@ -2,38 +2,79 @@ import { useState } from 'react';
 import {Navbar, Container, Nav} from 'react-bootstrap';
 import './App.css';
 import data from './data.js';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
+import Detail from './routes/Detail.js'
 
 function App() {
 
   let [shoes] = useState(data)
+  let navigate = useNavigate();
 
   return (
     <div className="App">
+
       <Navbar bg="light" variant="light">
         <Container>
           <Navbar.Brand href="#home">ShoesShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Cart</Nav.Link>
+          <Nav.Link onClick={()=>{ navigate('/') }}>Home</Nav.Link>
+          <Nav.Link onClick={()=>{ navigate('/detail') }}>Detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <div className="main-bg"></div>
-      <div className="container">
-        <div className="row">
-          
-          {
-            shoes.map((a, i)=>{
-              return (
-                <Card shoes={shoes[i]} i={i}></Card>
-              )
-            })
-          }
 
-        </div>
-      </div>
+
+
+      <Routes>
+        <Route path="/" element={
+          <>
+            <div className="main-bg"></div>
+            <div className="container">
+              <div className="row">
+                { shoes.map((a, i)=>{
+                  return <Card shoes={shoes[i]} i={i}></Card>
+                })}
+              </div>
+            </div>
+          </>
+        } />
+        <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
+        
+        <Route path="/about" element={<About />}>
+          <Route path="member" element={<div>멤버임</div>} />
+          <Route path="location" element={<About />} />
+        </Route>
+
+        <Route path="/event" element={<Event />}>
+          <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>} />
+          <Route path="two" element={<div>생일기념 쿠폰받기</div>} />
+          
+        </Route>
+
+      </Routes>
+
+
+      
     </div>
   );
+}
+
+function Event(){
+  return(
+    <div>
+      <h4>오늘의 이벤트</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+function About(){
+  return(
+    <div>
+      <h4>회사정보임</h4>
+      <Outlet></Outlet>
+    </div>
+  )
 }
 
 function Card(props){
