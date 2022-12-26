249795f297f6ad3370ce4a8f77257b95d044cd84
[wmwwm88] <[wmwwm88@gmail.com]>
2022-12-26 17:20:41 +0900

    -c

--- react/shop/src/routes/Detail.js
+++ react/shop/src/routes/Detail.js
@@ -1,13 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+
+
 
 function Detail(props){
 
+    let [count, setCount] = useState(0)
+    let [alert, setAlert] = useState(true)
+
+    useEffect(()=>{
+        let a = setTimeout(()=>{ setAlert(false)}, 2000);
+        return()=>{
+            // 기존타이머는 제거
+            clearTimeout(a)
+        }
+    })
+
     let {id} = useParams();
-    console.log(id);
 
     return (
         <div className="container">
+            {
+                alert == true
+                ? <div className="alert alert-warning">
+                    2초 이내 구매시 할인
+                </div>
+                : null
+            }
+            <button onClick={()=>{ setCount(count+1) }}>버튼</button>
             <div className="row">
                 <div className="col-md-6">
                     <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
