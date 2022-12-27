3a53a187a08fdfb092c9a3cf22046cda879dd875
wmwwm88.dev <wmwwm88@gmail.com>
2022-12-28 01:09:50 +0900

    -c

--- react/shop/src/App.js
+++ react/shop/src/App.js
@@ -4,10 +4,11 @@ import './App.css';
 import data from './data.js';
 import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
 import Detail from './routes/Detail.js'
+import axios from 'axios'
 
 function App() {
 
-  let [shoes] = useState(data)
+  let [shoes, setShoes] = useState(data)
   let navigate = useNavigate();
 
   return (
@@ -36,6 +37,16 @@ function App() {
                 })}
               </div>
             </div>
+            <button onClick={()=>{
+              axios.get('https://codingapple1.github.io/shop/data2.json')
+              .then((결과)=>{ 
+                let copy = [...shoes, ...결과.data];
+                setShoes(copy);
+              })
+              .catch(()=>{
+                console.log('실패함')
+              })
+            }}>버튼</button>
           </>
         } />
         <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
