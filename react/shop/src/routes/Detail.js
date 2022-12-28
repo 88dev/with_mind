064429e15c00c1ee2a4712c4b55e01b221043bea
wmwwm88.dev <wmwwm88@gmail.com>
2022-12-29 00:33:09 +0900

    -c

--- react/shop/src/routes/Detail.js
+++ react/shop/src/routes/Detail.js
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
+import { Nav } from 'react-bootstrap';
 
 
 function Detail(props){
 
     let [count, setCount] = useState(0)
     let [alert, setAlert] = useState(true)
+    let [탭, 탭변경] = useState(0)
+
 
     useEffect(()=>{
         let a = setTimeout(()=>{ setAlert(false)}, 2000);
@@ -39,8 +41,39 @@ function Detail(props){
                     <button className="btn btn-danger">주문하기</button> 
                 </div>
             </div>
+
+            <Nav variant="tabs" defaultActiveKey="link0">
+                <Nav.Item>
+                    <Nav.Link eventKey="link0" onClick={()=>{탭변경(0)}}>버튼0</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link1" onClick={()=>{탭변경(1)}}>버튼1</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link2" onClick={()=>{탭변경(2)}}>버튼2</Nav.Link>
+                </Nav.Item>
+            </Nav>
+            
+            <TabContent 탭={탭}/>
         </div> 
     )
+}
+
+function TabContent({탭}){
+
+    let [ fade, setFade ] = useState('') 
+
+    useEffect(()=>{
+        let a = setTimeout(()=>{ setFade('end') }, 10)
+        return()=>{
+            clearTimeout(a)
+            setFade('')
+        }
+    },[탭])
+
+    return (<div className={'start ' + fade}>
+        { [<div>내용0</div>,<div>내용1</div>,<div>내용2</div>][탭] }
+    </div>)
 }
 
 export default Detail;
