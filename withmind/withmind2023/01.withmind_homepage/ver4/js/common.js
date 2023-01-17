e22112910965ba3d0e983d97e65c442547169e91
[wmwwm88] <[wmwwm88@gmail.com]>
2023-01-17 12:21:13 +0900

    -c

--- withmind/withmind2023/01.withmind_homepage/ver4/js/common.js
+++ withmind/withmind2023/01.withmind_homepage/ver4/js/common.js
@@ -1,15 +1,15 @@
+AOS.init();
+
 function fnNav() {
     const nav = document.getElementById("nav");
     const gnb = document.getElementById("gnb");
     if (nav.checked == true){
         gnb.style.display = "block";
         $('header').addClass("on");
-        $('header').removeClass("off");
         $( 'body' ).css("overflow-y", "hidden");
         $('header').css('transition', '0s');
     } else {
         gnb.style.display = "none";
-        $('header').addClass("off");
         $('header').removeClass("on");
         $( 'body' ).css("overflow-y", "auto");
         $('header').css('transition', '0s');
@@ -17,24 +17,22 @@ function fnNav() {
 }
 
 $( window ).resize( function() {
-
-    $('header').addClass("off");
     $('header').removeClass("on");
     $('#nav').prop('checked', false);
     $('#gnb').css("display", "none");
 
     if($(window).width() > 1080) { 	
         $('#gnb').css("display", "flex");
-    } else if($(window).scrollTop()) {
-        $('header').removeClass("on");
-        $('header').addClass("off");
-        $('header').css('transition', '1s');
-    } 
+    }
 
-    
+    if($(window).scrollTop()) {
+        $('header').removeClass("on");
+        $('header').css('transition', '1s');
+    } else {
+        $('header').removeClass("on");
+    }
 });
 
-// menu
 $(document).ready(function() {
     $('#gnb li').click(function(){
         $( 'body' ).css("overflow-y", "auto");
@@ -67,6 +65,32 @@ $(document).ready(function() {
     $('.btn_contact').click(function(){
         var offset = $('#contact').offset();
         $('html').animate({scrollTop : offset.top}, 400);
+    });
+        
+    $(window).on('scroll',function(){
+        if($(window).scrollTop()){
+            $('header').addClass("on");
+            $('header').css('transition', '1s');
+        }else{
+            $('header').removeClass("on");
+            $('header').css('transition', '1s');
+        }
+    });
+    
+    var y = $(this).scrollTop();
+    if (y > 120) {
+        $('header').addClass("on");
+    } else{
+        $('header').removeClass("on");
+    }
+
+    // tab
+    $(".history_title li").click(function() {
+        var idx = $(this).index();
+        $(".history_title li").removeClass("on");
+        $(".history_title li").eq(idx).addClass("on");
+        $(".history_cont > section").hide();
+        $(".history_cont > section").eq(idx).show();
     });
 
     
