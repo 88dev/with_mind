28a44ea420fc587a4d59bbbd3efdf2e69905fa1a
wmwwm88.dev <wmwwm88@gmail.com>
2023-01-25 23:03:05 +0900

    -c

--- react/basic/src/basic/components/Products.jsx
+++ react/basic/src/basic/components/Products.jsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import { useEffect } from 'react';
 import { useState } from 'react';
 
 export default function Products() {
-    const [products, setProducts] = useState([]);
     const [checked, setChecked] = useState(false);
+    const [loading, error, products] = useState({ salesOnly: checked });
     const handleChange = () => setChecked((prev) => !prev);
 
-    useEffect(() => {
-        fetch(`data/${checked ? 'sale_' : ''}products.json`)
-        .then((res) => res.json())
-        .then((data) => {
-            console.log('🔥뜨끈한 데이터를 네트워크에서 받아옴');
-            setProducts(data);
-        });
-        return ()=>{
-            console.log('🧹 깨끗하게 청소하는 일들을 합니다.');
-        }
-    }, [checked]);
-    
+    if(loading) return <p>Loading...</p>;
+
+    if(error) return <p>{error}</p>;
+
     return (
         <>
             <input id="checkbox" type="checkbox" value={checked} onChange={handleChange} />
